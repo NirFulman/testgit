887e2962b4260438016a170e7a30478bce1efe24
User <User@10.0.0.5>
2020-04-14 15:37:46 +0300

    DD

--- src/testgit/WorldHello.java
+++ src/testgit/WorldHello.java
@@ -2,6 +2,6 @@ package testgit;
 
 public class WorldHello {
 public static void main(String[] args) {
-	System.out.println("hello git");
+	System.out.println("hell o git");
 }
 }
--- src/testgit/worldhellotwo.java
+++ src/testgit/worldhellotwo.java
@@ -2,6 +2,6 @@ package testgit;
 
 public class worldhellotwo {
 	public static void main(String[] args) {
-		System.out.println("we love the ben and son");
+		System.out.println("we  love the ben and son");
 	}
 }
